cff08553dddbefa898e758aaa743814c20882046
Solist-Alexander <fcco1233@gmail.com>
2024-03-01 12:47:58 +0200

    project-react

--- src/components/SearchContainer/SearchList.tsx
+++ src/components/SearchContainer/SearchList.tsx
@@ -5,22 +5,31 @@ import {movieService} from "../../services/movieService";
 import style from "../MoviesContainer/MoviesList/MoviesList.module.css";
 import MoviesListCard from "../MoviesContainer/MoviesListCard/MoviesListCard";
 import PaginationForMovie from "../PaginationForMovie/PaginationForMovie";
+import {useForm} from "react-hook-form";
+import {searchService} from "../../services/searchService";
 
 const SearchList = () => {
     const [movies, setMovies] = useState<IMovie[]>([])
     const [query, setQuery] = useSearchParams({page: '1'});
     const currentPage = query.get('page') ? query.get('page') : '1'
-
+    const {register, reset, handleSubmit, formState: {isValid, errors}, setValue} = useForm({
+        mode: "all"
+    })
 
     useEffect(() => {
         movieService.getAll(currentPage).then(({data}) => {
             setMovies(data.results)
         })
     }, [query])
+
+    const search = async (formData: any) => {
+        await searchService.getAll(formData.query, currentPage).then(({data}) => setMovies(data.results))
+    }
+
     return (
-        <div >
-            <form action="">
-                <input type="text" placeholder={'Поиск'} className={style.inputSearch}/>
+        <div>
+            <form onSubmit={handleSubmit(search)}>
+                <input type="text" placeholder={'Поиск'} name="query" className={style.inputSearch}/>
             </form>
 
             <div className={style.moviesListCardDiv}>
--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -6,3 +6,7 @@ import {urls} from "../constans/urls";
 const searchService = {
     getAll:(query:string, page:string|null = '1'):IRes<IMovieInfo> => apiService.get(urls.search.base(query), {params: {page}})
 }
+
+export {
+    searchService
+}
